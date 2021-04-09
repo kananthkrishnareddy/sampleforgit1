936026defc5b1bb4ad099318c03fe4fd16eabab4
ananth <kananthkrishnareddy@gmail.com>
2021-04-09 10:00:18 +0530

    first comitt

---
+++ src/main/java/sampleforgit/sampleforgit/sampleforgit/Application.java
@@ -0,0 +1,15 @@
+package sampleforgit.sampleforgit;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("sampleforgit.sampleforgit.mapper")
+public class Application {
+
+	public static void main(String[] args)  {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
